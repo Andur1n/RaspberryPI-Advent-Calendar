a0c6daf07b53f5e5ff8931d5e0ddcc069dc9a3e4
Andurin <33497687+Andur1n@users.noreply.github.com>
2025-12-08 21:40:20 +0000

    Create code.py

---
+++ Challenge 4/code.py
@@ -0,0 +1,22 @@
+##Little bit of code that allows you to fade the lights with the analogue knob. Will need to amend a bit in thhe future to include all LED's
+from machine import ADC, Pin, PWM
+import time
+
+potentiometer = ADC(Pin(27))
+
+led = PWM(Pin(18))
+
+led.freq(1000)
+
+reading = 0
+
+while True:
+    
+    reading = potentiometer.read_u16() 
+    
+    print(reading) 
+    
+    led.duty_u16(reading)
+    
+    time.sleep(0.001) 
+
