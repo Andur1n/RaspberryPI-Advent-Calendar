5b86ca7f9cff996bf10cf6a5a9910b1af4be2be2
Andurin <33497687+Andur1n@users.noreply.github.com>
2025-12-15 12:01:42 +0000

    Create code.py

---
+++ Challenge 6/code.py
@@ -0,0 +1,55 @@
+#Today a light sensor was added and we used the traffic light LED's to indicate how much light the sensor was "sensing". We then amended the code to include the buzzer from yesterday which in the below code goes off if the light sensor value drops below 30%
+# Imports
+from machine import ADC, Pin
+from machine import PWM, Pin
+
+import time
+
+# Set up the LED pins
+red = Pin(18, Pin.OUT)
+amber = Pin(19, Pin.OUT)
+green = Pin(20, Pin.OUT)
+
+# Define pin for our sensor
+lightsensor = ADC(Pin(26))
+
+#define buzzer on Pico H
+buzzer = PWM(Pin(13))
+buzzer.freq(1000)
+
+
+while True: # Run forever
+    
+    # Read sensor value and store it in a variable called 'light'
+    light = lightsensor.read_u16()
+
+    # Use the round function to limit the decimal places to 1
+    lightpercent = round(light/65535*100,1)
+
+    # Print our reading percentage with % symbol
+    print(str(lightpercent) +"%")
+    
+    # 1 second delay between readings
+    time.sleep(1)
+
+    if lightpercent <= 30: # If percentage is less than or equal to 30
+        
+        red.value(1) # Red LED on
+        amber.value(0)
+        green.value(0)
+        buzzer.duty_u16(10000)
+        
+
+    elif 30 < lightpercent < 60: # If percentage is between 30 and 60
+        
+        red.value(0)
+        amber.value(1) # Amber LED on
+        green.value(0)
+        buzzer.duty_u16(0)
+
+    elif lightpercent >= 60: # If percentage is greater than or equal to 60
+        
+        red.value(0)
+        amber.value(0)
+        green.value(1) # Green LED on
+        buzzer.duty_u16(0)
