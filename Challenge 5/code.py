b26fc1c21c10f9eba694172faabf431a54660fe9
Andurin <33497687+Andur1n@users.noreply.github.com>
2025-12-15 11:32:02 +0000

    Create code.py

---
+++ Challenge 5/code.py
@@ -0,0 +1,36 @@
+#today a little speaker was added to our pico H breadboard. We then learned how to control the speaker and how to minimise the code in order to get it to play songs.
+
+from machine import Pin, PWM
+import time
+
+buzzer = PWM(Pin(13)) 
+
+C = 523
+D = 587
+E = 659
+G = 784
+
+volume = 32768
+
+def playtone(note,vol,delay1,delay2):
+    buzzer.duty_u16(vol)
+    buzzer.freq(note)
+    time.sleep(delay1)
+    buzzer.duty_u16(0)
+    time.sleep(delay2)
+    
+playtone(E,volume,0.1,0.2)
+playtone(E,volume,0.1,0.2)
+playtone(E,volume,0.1,0.5) 
+
+playtone(E,volume,0.1,0.2)
+playtone(E,volume,0.1,0.2)
+playtone(E,volume,0.1,0.5) 
+
+playtone(E,volume,0.1,0.2)
+playtone(G,volume,0.1,0.2)
+playtone(C,volume,0.1,0.2)
+playtone(D,volume,0.1,0.2)
+playtone(E,volume,0.1,0.2)
+
+buzzer.duty_u16(0)
